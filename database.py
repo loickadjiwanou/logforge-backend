89e1c3c709679d14dca555fc72bc6bc2835a07fd
Loick Adjiwanou <loick.adj@gmail.com>
2026-07-24 17:47:33 +0100

    refactor: improve MongoDB connection setup and environment variable handling

--- database.py
+++ database.py
@@ -1,11 +1,21 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 from pathlib import Path
+from urllib.parse import quote_plus
 import os
 
 ROOT_DIR = Path(__file__).parent
-load_dotenv(ROOT_DIR / '.env')
+load_dotenv(ROOT_DIR / ".env")
 
-mongo_url = os.environ['MONGO_URL']
+user = quote_plus(os.getenv("MONGO_USER"))
+password = quote_plus(os.getenv("MONGO_PASSWORD"))
+cluster = os.getenv("MONGO_URL")
+db_name = os.getenv("DB_NAME")
+
+mongo_url = (
+    f"mongodb+srv://{user}:{password}@{cluster}/"
+    f"{db_name}?retryWrites=true&w=majority"
+)
+
 client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+db = client[db_name]
